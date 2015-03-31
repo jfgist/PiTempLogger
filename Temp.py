84c55e9394d57e390ff2d507661307ede3328274
jfgist <James.Gist@gmail.com>
2015-03-31 02:40:53 +0000

    updated temperature code

--- Temp.py
+++ Temp.py
@@ -1,50 +1,59 @@
 import os, glob, time, gspread, sys, datetime
  
 #Google account details
-email = 'foo.bar@gmail.com'
-password = 'Foo_Bars_Password'
-spreadsheet = 'Temperature_log' #the name of the spreadsheet already created
- 
-#attempt to log in to your google account
-try:
-    gc = gspread.login(email,password)
-except:
-    print('fail')
-    sys.exit()
- 
-#open the spreadsheet
-worksheet = gc.open(spreadsheet).sheet1
+email = ''
+password = ''
+spreadsheet = 'Temp_Log1' #the name of the spreadsheet already created 
  
 #initiate the temperature sensor
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 #set up the location of the sensor in the system
-device_folder = glob.glob('/sys/bus/w1/devices/28*')
-device_file = [device_folder[0] + '/w1_slave', device_folder[1] + '/w1_slave']
+print('get device folder')
+device_folder = glob.glob('/sys/bus/w1/devices/28*')[0]
+device_file = device_folder + '/w1_slave'
  
  
 def read_temp_raw(): #a function that grabs the raw temperature data from the sensor
-    f_1 = open(device_file[0], 'r')
-    lines_1 = f_1.readlines()
-    f_1.close()
-    f_2 = open(device_file[1], 'r')
-    lines_2 = f_2.readlines()
-    f_2.close()
-    return lines_1 + lines_2
+    print('read temperature')
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
  
  
 def read_temp(): #a function that checks that the connection was good and strips out the temperature
     lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES' or lines[2].strip()[-3:] != 'YES':
+    while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t='), lines[3].find('t=')
-    temp = float(lines[1][equals_pos[0]+2:])/1000, float(lines[3][equals_pos[1]+2:])/1000
-    return temp
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return temp_f
  
 while True: #infinite loop
-    temp = read_temp() #get the temp
-    values = [datetime.datetime.now(), temp[0], temp[1]]
-    worksheet.append_row(values) #write to the spreadsheet
-    time.sleep(600) #wait 10 minutes
+
+    try:
+        # login to google account
+        gc = gspread.login(email,password)  
+
+        # open the spreadsheet
+        worksheet = gc.open(spreadsheet).sheet1
+        
+        # get the temp
+        temp = read_temp()
+        values = [datetime.datetime.now(), temp]
+
+        print('values')
+        print(values)
+        print('write values to spreadsheet')
+
+        worksheet.append_row(values) #write to the spreadsheet
+        time.sleep(900) #wait 15 minutes
+
+    except gspread.AuthenticationError:
+        print('login failed')
